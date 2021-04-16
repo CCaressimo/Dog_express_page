d1d57f6489bb545a190d916899605c24ac4b5548
chris <c.caressimo@gmail.com>
2021-04-15 21:34:05 -0400

    added more breeds and linked

--- db.js
+++ db.js
@@ -1,34 +1,58 @@
 module.exports = [
-    {
-        name: "Kane",
-        image: 'https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12213218/German-Shepherd-on-White-00.jpg',
-        breed: "German Shephard"
-    },
-
-    {
-        name: "",
-        image
-    }
+  {
+    name: "Ifrit",
+    image:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12213218/German-Shepherd-on-White-00.jpg",
+    breed: "German Shephard",
+  },
 
   {
-    name: "Frenchie",
+    name: "Balthazar",
+    image:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/26155623/Siberian-Husky-standing-outdoors-in-the-winter.jpg",
+    breed: "Siberian Husky",
+  },
+
+  {
+    name: "Bahamut",
+    image:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12230849/Mastiff-On-White-03.jpg",
+    breed: "Mastiff",
+  },
+
+  {
+    name: "Azazel",
+    image:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/26235316/Border-Collie-laying-down.jpg",
+    breed: "Border Collie",
+  },
+
+  {
+    name: "Fenriz",
+    image:
+      "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2015/05/22210853/Alaskan-Malamutes-pulling-a-sled-over-the-snow.jpg",
+    breed: "Alaskan Malamute",
+  },
+
+  {
+    name: "Shaitan",
     image:
       "https://dogtime.com/assets/uploads/gallery/french-bulldog-dog-breed-pictures/1-puppy.jpg",
     breed: "French Bulldog",
   },
   {
-    name: "Buu",
+    name: "Behemoth",
     image: "https://cdn.orvis.com/images/DBS_ShibaInu_1280.jpg",
     breed: "Shiba Inu",
   },
   {
-    name: "Aussie",
+    name: "Moloch",
     image:
       "https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/03173132/Australian-Shepherd.1.jpg",
     breed: "Australian Shephard",
   },
   {
-    name: "Tim",
+    name: "Midgard",
     image:
       "https://www.cesarsway.com/wp-content/uploads/2019/02/Rottweiler-1024x689.jpeg",
     breed: "Rottweiler",
